b6ea3a41d42fb1be22dc242a4d159513cb129bee
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2019-12-08 23:08:27 -0300

    cli: update deployed court addresses

--- packages/cli/truffle-config.js
+++ packages/cli/truffle-config.js
@@ -1,9 +1,9 @@
 const config = require('@aragon/truffle-config-v5/truffle-config')
 const { networks: { rpc, ropsten, rinkeby, mainnet } } = config
 
-rpc.court = '0x21a59654176f2689d12E828B77a783072CD26680'
+rpc.court = undefined
 ropsten.court = '0x3b26bc496aebaed5b3E0E81cDE6B582CDe71396e'
-rinkeby.court = undefined
+rinkeby.court = '0x4ed496172778DbaBaee6393BA99f78E4e1f07821'
 mainnet.court = '0xee4650cBe7a2B23701D416f58b41D8B76b617797'
 
 module.exports = config
