2507582dacb0d38db46805c287bdaa1dc5a5ed4d
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2019-12-20 16:56:43 -0300

    server: small enhancements

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -9,7 +9,7 @@ const corsOptions = {
   origin: function (origin, callback) {
     const whitelist = process.env.CORS_WHITELIST.split(',')
     if (whitelist.indexOf(origin) !== -1) callback(null, true)
-    else callback(new Error('Not allowed by CORS'))
+    else callback(new Error(`Origin '${origin}' not allowed by CORS`))
   }
 }
 
--- packages/server/src/helpers/error-handler.js
+++ packages/server/src/helpers/error-handler.js
@@ -2,6 +2,11 @@ export default (err, req, res, next) => {
   if (res.headersSent) {
     return next(err)
   }
+
+  if (err instanceof SyntaxError) {
+    return res.status(400).send({ errors: 'Make sure your request is a well formed JSON' })
+  }
+
   console.error(err.stack)
   res.status(500).send('Something went wrong :(')
 }
