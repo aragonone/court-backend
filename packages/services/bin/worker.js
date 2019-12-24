8a844606e197531c86786d3e915784881ad61f7a
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2019-12-24 00:09:41 -0300

    service: implement heartbeat worker

--- packages/services/bin/worker.js
+++ packages/services/bin/worker.js
@@ -5,11 +5,12 @@ import errorHandler from '../src/helpers/error-handler'
 import sleep from '@aragon/court-backend-shared/helpers/sleep'
 import Logger from '@aragon/court-backend-shared/helpers/logger'
 
-let [workerPath, name, times, repeat] = process.argv.slice(2)
+let [workerPath, name, times, tries, repeat] = process.argv.slice(2)
 if (!workerPath) throw Error('Cannot start worker with missing path')
 if (name === undefined) name = 'unknown'
 
 times = (times === undefined) ? 1 : parseInt(times)
+tries = (tries === undefined) ? 1 : parseInt(tries)
 repeat = (repeat === undefined) ? 0 : parseInt(repeat)
 
 Logger.setDefaults(false, true)
@@ -22,7 +23,7 @@ async function run() {
   for (let job = 1; job !== times; job++) {
     try {
       logger.info(`Creating job #${job}`)
-      await worker(logger)
+      await worker(name, job, tries, logger)
       logger.success(`Job #${job} finished successfully`)
     } catch (error) {
       logger.error(`Job #${job} exited with error: ${error.message}`)
--- packages/services/src/workers/heartbeat.js
+++ packages/services/src/workers/heartbeat.js
@@ -1,5 +1,31 @@
-export default async function (logger) {
-  logger.success('hello')
+import sleep from '@aragon/court-backend-shared/helpers/sleep'
+import Models from '@aragon/court-backend-server/build/models'
+import Network from '@aragon/court-backend-server/build/web3/Network'
 
+const SECONDS_BETWEEN_INTENTS = 3
+const MAX_TRANSITIONS_PER_CALL = 2
 
+export default async function (worker, job, tries, logger) {
+  try {
+    await heartbeat(logger, tries)
+  } catch (error) {
+    const { ErrorLogs } = Models
+    await ErrorLogs.create({ context: `Worker '${worker}' job #${job}`, message: error.message, stack: error.stack })
+    throw error
+  }
+}
+
+async function heartbeat(logger, tries, intent = 1) {
+  try {
+    logger.success(`Requesting the Court to transition up-to ${MAX_TRANSITIONS_PER_CALL} terms, try #${intent}`)
+    const court = await Network.getCourt()
+    const transitions = await court.heartbeat(MAX_TRANSITIONS_PER_CALL)
+    logger.success(`Transitioned ${transitions} Court terms`)
+  } catch (error) {
+    logger.error('Failed to transition terms with error')
+    console.error(error)
+    if (intent === tries) throw error
+    await sleep(SECONDS_BETWEEN_INTENTS)
+    await heartbeat(logger, tries, intent + 1)
+  }
 }
