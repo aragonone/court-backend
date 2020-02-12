d63c198a6ce92c9a6f8bdf02cf7856c62454b548
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2020-02-12 16:21:16 -0300

    App: Improve faucet messaging (#72)

--- packages/app/src/reducers/faucet.js
+++ packages/app/src/reducers/faucet.js
@@ -7,11 +7,11 @@ const AccountsReducer = (state = initialState, action) => {
     case ActionTypes.RECEIVE_FAUCET:
       return Object.assign({}, state, { address: action.address })
     case ActionTypes.RECEIVE_FAUCET_ANJ_BALANCE:
-      return Object.assign({}, state, { anj: { symbol: action.symbol, balance: action.balance, address: action.address }})
+      return Object.assign({}, state, { anj: { symbol: action.symbol, balance: action.balance, address: action.address, period: action.period, quota: action.quota }})
     case ActionTypes.RECEIVE_FAUCET_ANT_BALANCE:
-      return Object.assign({}, state, { ant: { symbol: action.symbol, balance: action.balance, address: action.address }})
+      return Object.assign({}, state, { ant: { symbol: action.symbol, balance: action.balance, address: action.address, period: action.period, quota: action.quota }})
     case ActionTypes.RECEIVE_FAUCET_FEE_BALANCE:
-      return Object.assign({}, state, { fee: { symbol: action.symbol, balance: action.balance, address: action.address }})
+      return Object.assign({}, state, { fee: { symbol: action.symbol, balance: action.balance, address: action.address, period: action.period, quota: action.quota }})
     default:
       return state
   }
