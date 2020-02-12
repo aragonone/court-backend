cd1b01efd8d85e6ebb49130ef669ac2ca05ef0c0
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2020-02-12 10:49:54 -0300

    App: Remove errors stuff (#69)

--- packages/app/src/components/admin/RevealsList.react.js
+++ packages/app/src/components/admin/RevealsList.react.js
@@ -33,7 +33,6 @@ export default class RevealsList extends React.Component {
                 <th>Outcome</th>
                 <th>Salt</th>
                 <th>Revealed</th>
-                <th>Tries</th>
                 <th>Error</th>
                 <th>Created at</th>
               </tr>
@@ -59,8 +58,6 @@ export default class RevealsList extends React.Component {
           <td>{reveal.outcome}</td>
           <td>{summarize(reveal.salt)}</td>
           <td>{reveal.revealed ? 'Yes' : 'No'}</td>
-          <td>{reveal.tries}</td>
-          <td>{reveal.error ? <Link to={`/errors/${reveal.error.id}`}>{summarize(reveal.error.message)}</Link> : ''}</td>
           <td>{reveal.createdAt}</td>
         </tr>
       )
--- packages/app/src/reducers/admin.js
+++ packages/app/src/reducers/admin.js
@@ -1,6 +1,6 @@
 import * as ActionTypes from '../actions/types'
 
-const initialState = { id: null, email: null, token: null, admins: [], reveals: [], settlements: [], subscriptions: [], errors: [], error: {} }
+const initialState = { id: null, email: null, token: null, admins: [], reveals: [], subscriptions: [] }
 
 const AdminReducer = (state = initialState, action) => {
   switch(action.type) {
@@ -16,14 +16,8 @@ const AdminReducer = (state = initialState, action) => {
       return Object.assign({}, state, { admins: action.admins })
     case ActionTypes.RECEIVE_ADMIN_REVEALS:
       return Object.assign({}, state, { reveals: action.reveals })
-    case ActionTypes.RECEIVE_ADMIN_SETTLEMENTS:
-      return Object.assign({}, state, { settlements: action.settlements })
     case ActionTypes.RECEIVE_ADMIN_SUBSCRIPTIONS:
       return Object.assign({}, state, { subscriptions: action.subscriptions })
-    case ActionTypes.RECEIVE_ADMIN_ERRORS:
-      return Object.assign({}, state, { errors: action.errors })
-    case ActionTypes.RECEIVE_ADMIN_ERROR:
-      return Object.assign({}, state, { error: action.error })
     default:
       return state
   }
