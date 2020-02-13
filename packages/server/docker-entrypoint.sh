471045bf1d00af0ff68802d95055ffe2f4f1ba03
ßingen <bingen@aragon.one>
2020-02-13 10:30:34 +0100

    Dockerize server and services (#55)
    * Server: add production settings
    
    * Server: add dockerfile
    
    * server/services: move and create docker files
    
    * server: Make docker image start more robust
    
    * server: allow different db names for sequelize db:create
    
    Co-authored-by: Brett Sun <qisheng.brett.sun@gmail.com>
    Co-authored-by: Martynas Prokopas <promaty@gmail.com>

---
+++ packages/server/docker-entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# wait for DB
+db_result=0
+while [ "${db_result}" -ne "1" ]; do
+    echo "Waiting for DB..."
+    # npx sequelize db:migrate:status 2>&1 | grep 'connect ECONNREFUSED' > /dev/null
+    npx sequelize db:migrate:status 2>&1 | grep 'connect ECONNREFUSED'
+    db_result=$?
+    if [ "${db_result}" -eq "0" ]; then
+        sleep 5
+    fi
+done
+
+echo "DB is up!"
+
+# Check DB creation
+#npx sequelize db:migrate:status 2>&1 | grep 'database .* does not exist' > /dev/null
+npx sequelize db:migrate:status 2>&1 | grep 'database .* does not exist'
+result=$?
+if [ "${result}" -eq "0" ]; then
+    echo "Creating Database"
+    npx sequelize db:create
+    create_result=$?
+    if [ "${create_result}" -ne "0" ]; then
+        echo "Error creating Database"
+        exit 1
+    fi
+fi
+
+# Check DB migration
+npx sequelize db:migrate:status | grep "down" > /dev/null
+result=$?
+if [ "${result}" -eq "0" ]; then
+    echo "Migrating Database"
+    npx sequelize db:migrate
+    migrate_result=$?
+    if [ "${migrate_result}" -ne "0" ]; then
+        echo "Error migrating Database"
+        exit 1
+    fi
+fi
+
+echo "Seeding Database"
+npx sequelize db:seed:all > /dev/null 2>&1
+
+# npm start
+exec "$@"
---
+++ packages/server/src/index.js
@@ -0,0 +1,51 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { createMiddleware, signalIsUp } from '@promster/express'
+import { createServer } from '@promster/server'
+
+import errorHandler from './helpers/error-handler'
+import routes from './routes'
+
+dotenv.config()
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    const whitelist = process.env.CORS_WHITELIST.split(',')
+    if (whitelist.indexOf(origin) !== -1) callback(null, true)
+    else callback(new Error(`Origin '${origin}' not allowed by CORS`))
+  }
+}
+
+
+const app = express()
+app.use(createMiddleware({ app }));
+app.use(helmet())
+app.use(cors(corsOptions))
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+routes(app)
+
+app.get('*', (request, response) => response.status(200).send({ message: 'Welcome to Aragon Court server' }))
+
+app.use(errorHandler)
+
+const port = process.env.PORT || 8000
+app.listen(port, (err) => {
+  if (err) return console.error(err)
+  signalIsUp()
+  console.log(`Listening on port ${port}`)
+})
+
+const metricsPort = process.env.METRICS_PORT || 8001
+createServer({ port: metricsPort }).then(server =>
+  console.log(`@promster/server started on port ${metricsPort}.`)
+)
