5e96d410cd67b4fc976c45c116561f30c1550b2b
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2020-03-18 16:36:12 -0300

    Services: Fix error handler (#91)
    * services: fix error handler
    
    * cli: rename error handler file

--- packages/services/src/helpers/error-handler.js
+++ packages/services/src/helpers/error-handler.js
@@ -1,7 +1,6 @@
-export default (err, req, res, next) => {
-  if (res.headersSent) {
-    return next(err)
-  }
-  console.error(err.stack)
-  res.status(500).send('Something went wrong :(')
+export default error => {
+  logger.error(`Process finished with error:`)
+  console.log(error)
+  console.log()
+  process.exit(1)
 }
