d64734873064b858b8ae4c45a26e3784008cc89a
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-03-19 16:21:50 -0300

    services: fix heartbeat retries

--- packages/services/src/workers/heartbeat.js
+++ packages/services/src/workers/heartbeat.js
@@ -1,6 +1,7 @@
 import sleep from '@aragonone/court-backend-shared/helpers/sleep'
 import Network from '@aragonone/court-backend-server/build/web3/Network'
 
+const HEARBEAT_TRIES_PER_JOB = 3
 const SECONDS_BETWEEN_INTENTS = 3
 const MAX_TRANSITIONS_PER_CALL = 20
 
@@ -20,8 +21,8 @@ async function heartbeat(logger, court, attempt = 1) {
     const transitions = await court.heartbeat(MAX_TRANSITIONS_PER_CALL)
     logger.success(`Transitioned ${transitions} Court terms`)
   } catch (error) {
-    logger.error('Failed to transition terms with error')
-    console.error(error)
+    logger.error('Failed to transition terms with error', error)
+    if (attempt >= HEARBEAT_TRIES_PER_JOB) return
     await sleep(SECONDS_BETWEEN_INTENTS)
     await heartbeat(logger, court, attempt + 1)
   }
