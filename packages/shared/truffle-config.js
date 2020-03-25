ba299e37e3daba0b67ea4f3cf8d8486d616f34b9
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-03-25 12:30:25 -0300

    Chore: Update court staging address

--- packages/shared/truffle-config.js
+++ packages/shared/truffle-config.js
@@ -7,7 +7,7 @@ config.networks.usability = Object.assign({}, { ...config.networks.rinkeby })
 const { networks: { rpc, ropsten, rinkeby, usability, staging, mainnet } } = config
 
 rpc.court = undefined
-staging.court = '0xd0dcfc6b5b36f7e77f3daa2d9031b241651a6916'
+staging.court = '0x52180af656a1923024d1accf1d827ab85ce48878'
 ropsten.court = '0x3b26bc496aebaed5b3E0E81cDE6B582CDe71396e'
 rinkeby.court = '0xb5ffbe75fa785725eea5f931b64fc04e516c9c5d'
 usability.court = '0x44f788370206696b20B94BC77c4f73Ca264aa05E'
