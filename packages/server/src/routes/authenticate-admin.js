51a1b15a1a0ccebf5720189d62985b3bb2f41970
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-04-08 12:47:24 -0300

    server: avoid unsetting cookies on admin authentication

--- packages/server/src/routes/authenticate-admin.js
+++ packages/server/src/routes/authenticate-admin.js
@@ -5,14 +5,8 @@ import asyncMiddleware from '../helpers/async-middleware'
 const { Admin } = Models
 
 const authenticate = route => async (request, response, next) => {
-  const { session, cookies } = request
-
-  if (!session.modelId || !session.modelType) {
-    // Check if user's cookie is still saved in browser and user is not set, then automatically log the user out
-    // This usually happens when you stop your express server after login, your cookie still remains saved in the browser
-    if (cookies && cookies.aragonCourtSessionID) response.clearCookie('aragonCourtSessionID')
-    throw HttpError._403({ error: 'Unauthorized' })
-  }
+  const { session } = request
+  if (!session.modelId || !session.modelType) throw HttpError._403({ error: 'Unauthorized' })
 
   request.currentAdmin = await Admin.findByPk(session.modelId)
   route(request, response, next)
