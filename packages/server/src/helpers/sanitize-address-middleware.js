0a257db06dcec694f731b464f869cd21f1cf81da
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2020-04-08 12:56:01 -0300

    Server: Sanitize addresses for all endpoints (#136)
    * server: sanitize addresses
    
    * server: optimize sanitize address middleware

---
+++ packages/server/src/helpers/sanitize-address-middleware.js
@@ -0,0 +1,14 @@
+import { isAddress } from 'web3-utils'
+
+const sanitizeAddresses = object => {
+  Object.entries(object).forEach(([key, value]) => {
+    if (!!value && typeof value === 'object') sanitizeAddresses(value)
+    else if(isAddress(value)) object[key] = value.toLowerCase()
+  })
+}
+
+export default () => (req, res, next) => {
+  sanitizeAddresses(req.query)
+  sanitizeAddresses(req.body)
+  next()
+}
