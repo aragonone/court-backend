0c5f95246e79ecdb0a050029e16b06e2c3077a40
Martynas Prokopas <promaty@gmail.com>
2020-04-08 16:32:47 +0100

    Server: dummy REST endpoints for notifications (#132)
    * implemented dummy REST endpoints for notifications
    
    * typo
    
    * removed keys from json keys
    
    * cleaner destructuring
    
    * reversed notifications logic from enabled to disabled
    
    * minor updates according to js guidelines
    
    * typo
    
    * removed email exception in auth, will use different endpoint
    
    * renamed typoed dashboards name

--- packages/server/src/controllers/UsersController.js
+++ packages/server/src/controllers/UsersController.js
@@ -4,7 +4,141 @@ import UsersValidator from '../validators/UsersValidator'
 
 const { User, UserAddress } = Models
 
+// temp dummy models
+let Users = { }
+let UserEmails = { }
+let UserSessions = { }
+
+
 export default {
+  async details(req, res) {
+    const { params: { address } } = req
+    const body = {
+      emailExists: false,
+      emailVerified: false,
+      addressVerified: false,
+      notificationsDisabled: false,
+      ... Users[address]
+    }
+    res.send(body)
+  },
+
+
+  sessions: {
+    async create(req, res) {
+      const { params: { address } } = req
+      if (!Users.hasOwnProperty(address)) {
+        Users[address] = {}
+      }
+      Users[address]['addressVerified'] = true
+      if (!UserSessions.hasOwnProperty(address)) {
+        UserSessions[address] = {}
+      }
+      UserSessions[address][req.session.id] = true
+      const body = {
+        authenticated: true
+      }
+      res.send(body)
+    },
+
+    async authenticate(req,res,next) {
+      const { params: { address } } = req
+      if (
+        !UserSessions.hasOwnProperty(address) ||
+        !UserSessions[address].hasOwnProperty(req.session.id)
+      ) {
+        const errors = [{access: `Unauthorized, please authenticate at /users/${address}/sessions`}]
+        throw HttpError._403({ errors })
+      }
+      next()
+    },
+    
+    async deleteCurrent(req, res) {
+      const { params: { address } } = req
+      delete UserSessions[address][req.session.id]
+      const body = {
+        deleted: true
+      }
+      res.send(body)
+    },
+    
+    async deleteAll(req, res) {
+      const { params: { address } } = req
+      delete UserSessions[address]
+      const body = {
+        deleted: true
+      }
+      res.send(body)
+    },
+  },
+
+
+  email: {
+    async get(req, res) {
+      const { params: { address } } = req
+      const body = {
+        email: UserEmails[address],
+      }
+      res.send(body)
+    },
+
+    async change(req, res) {
+      const { params: { address } } = req
+      UserEmails[address] = req.body.email
+      if (!Users.hasOwnProperty(address)) {
+        Users[address] = {}
+      }
+      Users[address]['emailExists'] = true
+      const body = {
+        email: req.body.email,
+        sent: true
+      }
+      res.send(body)
+    },
+    
+    async verify(req, res) {
+      const { params: { address } } = req
+      Users[address]['emailVerified'] = true
+      const body = {
+        verified: true
+      }
+      res.send(body)
+    },
+    
+    async send(req, res) {
+      const { params: { address } } = req
+      const body = {
+        sent: true
+      }
+      res.send(body)
+    },
+    
+    async delete(req, res) {
+      const { params: { address } } = req
+      delete UserEmails[address]
+      Users[address]['emailExists'] = false
+      Users[address]['emailVerified'] = false
+      Users[address]['notificationsDisabled'] = false // deleting notifications table entry
+      const body = {
+        deleted: true
+      }
+      res.send(body)
+    },
+  },
+
+
+  notifications: { 
+    async change(req, res) {
+      const { params: { address } } = req
+      Users[address]['notificationsDisabled'] = req.body.disabled
+      const body = {
+        disabled: req.body.disabled
+      }
+      res.send(body)
+    },
+  },
+
+
   async exists(request, response) {
     const address = request.params.address.toLowerCase()
     const exists = await UserAddress.exists(address)
--- packages/server/src/routes/index.js
+++ packages/server/src/routes/index.js
@@ -5,6 +5,27 @@ import { admins, users, reveals } from '../controllers'
 export default app => {
   app.get('/', (request, response) => response.status(200).send({ message: 'Welcome to Aragon Court server' }))
 
+  // check user details
+  app.get(    '/users/:address',                      asyncMiddleware(users.details))
+
+  // manage user sessions
+  app.post(   '/users/:address/sessions',             asyncMiddleware(users.sessions.create))
+  app.all(    '/users/:address/*',                    asyncMiddleware(users.sessions.authenticate))    // requests below need an authenticated session
+  app.delete( '/users/:address/sessions[:]current',   asyncMiddleware(users.sessions.deleteCurrent))
+  app.delete( '/users/:address/sessions',             asyncMiddleware(users.sessions.deleteAll))
+
+  // manage user emails
+  app.get(    '/users/:address/email',                asyncMiddleware(users.email.get))
+  app.put(    '/users/:address/email',                asyncMiddleware(users.email.change))
+  app.post(   '/users/:address/email[:]verify',       asyncMiddleware(users.email.verify))
+  app.post(   '/users/:address/email[:]send',         asyncMiddleware(users.email.send))
+  app.delete( '/users/:address/email',                asyncMiddleware(users.email.delete))
+
+  // set notifications
+  app.put(    '/users/:address/notifications',        asyncMiddleware(users.notifications.change))
+
+
+  // other endpoints
   app.post('/login', asyncMiddleware(admins.login))
 
   app.get('/user/:address', asyncMiddleware(users.exists))
