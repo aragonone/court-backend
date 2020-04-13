c9edb937de9c26220f37359e0ec55b8b816c6a38
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2020-04-13 11:04:31 -0300

    Server: Send errors always as a list (#141)

--- packages/server/src/routes/authenticate-admin.js
+++ packages/server/src/routes/authenticate-admin.js
@@ -6,7 +6,7 @@ const { Admin } = Models
 
 const authenticate = route => async (request, response, next) => {
   const { session } = request
-  if (!session.modelId || !session.modelType) throw HttpError._403({ error: 'Unauthorized' })
+  if (!session.modelId || !session.modelType) throw HttpError._403({ errors: [{ status: 'Unauthorized' }] })
 
   request.currentAdmin = await Admin.findByPk(session.modelId)
   route(request, response, next)
