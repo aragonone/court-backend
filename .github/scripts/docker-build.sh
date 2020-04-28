86843ada5577380ecccc2f4473da626ebf09c4a1
Martynas Prokopas <promaty@gmail.com>
2020-04-28 20:16:25 +0100

    Chore: Moved CI/CD logic into shared scripts (#144)

--- .github/scripts/docker-build.sh
+++ .github/scripts/docker-build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # use previously built image for cache if possible
+REPO="$1"
+GITHUB_SHA="$2"
 docker pull $REPO:latest || true
 docker pull $REPO:rolling || true
 docker build . -t $REPO:${GITHUB_SHA} -t $REPO:rolling --cache-from $REPO:rolling --cache-from $REPO:latest
--- .github/scripts/test-server/run.sh
+++ .github/scripts/test-server/run.sh
@@ -2,7 +2,7 @@
 DIR=$(dirname "$0")
 cp .env.sample $DIR/.env
 cd $DIR
-export SERVER_IMAGE=$REPO:${GITHUB_SHA}
+export SERVER_IMAGE="$1"
 docker-compose up -d
 sleep 10
 docker-compose exec -T court_server npm run test:server
