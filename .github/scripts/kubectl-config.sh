8700338c79a488e6b9c9c58427459994daf3f95f
Martynas Prokopas <promaty@gmail.com>
2020-04-28 22:12:02 +0200

    Chore: Fixed kubectl config CI/CD shell script

--- .github/scripts/kubectl-config.sh
+++ .github/scripts/kubectl-config.sh
@@ -5,6 +5,6 @@ KUBE_TOKEN="$3"
 # config cluster access
 echo $KUBE_CA | base64 -d > ca.crt
 kubectl config set-cluster aragon --server=$KUBE_SERVER --certificate-authority=ca.crt
-kubectl config set-credentials aragon --token=$(base64 -d <<< $KUBE_TOKEN)
+kubectl config set-credentials aragon --token=$(echo $KUBE_TOKEN | base64 -d)
 kubectl config set-context aragon --cluster=aragon --user=aragon
 kubectl config use-context aragon
