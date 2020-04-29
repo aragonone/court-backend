67e38dcb3b750098fcfb4cf216124e61d42fb775
Martynas Prokopas <promaty@gmail.com>
2020-04-30 01:32:57 +0200

    added missing index file

---
+++ packages/server/src/models/objection/index.js
@@ -0,0 +1,17 @@
+import Admins from './Admins'
+import Reveals from './Reveals'
+import Sessions from './Sessions'
+import UserEmails from './UserEmails'
+import UserEmailVerificationTokens from './UserEmailVerificationTokens'
+import UserNotificationSettings from './UserNotificationSettings'
+import Users from './Users'
+
+export {
+  Admins,
+  Reveals,
+  Sessions,
+  UserEmails,
+  UserEmailVerificationTokens,
+  UserNotificationSettings,
+  Users,
+}
