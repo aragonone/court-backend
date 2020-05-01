ed45efbc6576ac2cf43c44d94e1552eb67ff333c
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-05-01 20:14:14 -0300

    server: sanitize addresses after params are parsed

--- packages/server/src/helpers/async-middleware.js
+++ packages/server/src/helpers/async-middleware.js
@@ -1,3 +1,6 @@
+import sanitizeAddressMiddleware from './sanitize-address-middleware'
+
 export default fn => (req, res, next) => {
+  sanitizeAddressMiddleware(req)
   Promise.resolve(fn(req, res, next)).catch(next)
 }
