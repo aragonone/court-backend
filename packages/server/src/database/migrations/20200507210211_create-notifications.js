baaf3e04bf9af607fbc3b9b91cbf52c98b7a5f09
Martynas Prokopas <promaty@gmail.com>
2020-05-07 22:01:06 +0200

    added not nullabel

--- packages/server/src/database/migrations/20200507210211_create-notifications.js
+++ packages/server/src/database/migrations/20200507210211_create-notifications.js
@@ -5,7 +5,7 @@ export function up(knex) {
       'notificationType1',
       'notificationType2',
     ]).notNullable()
-    table.integer('userId').index()
+    table.integer('userId').index().notNullable()
     table.foreign('userId').references('Users.id').onDelete('CASCADE')
     table.datetime('createdAt').defaultTo(knex.fn.now()).notNullable()
     table.datetime('updatedAt').defaultTo(knex.fn.now()).notNullable()
