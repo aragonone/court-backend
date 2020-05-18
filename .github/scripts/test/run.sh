6415f92ed4b1bd5ff06ac40e2a9dbc95f3a1848d
Martynas Prokopas <promaty@gmail.com>
2020-05-18 20:00:29 +0200

    added more reliable test script

--- .github/scripts/test/run.sh
+++ .github/scripts/test/run.sh
@@ -4,7 +4,7 @@ cp .env.sample $DIR/.env
 cd $DIR
 export SERVER_IMAGE="$1"
 docker-compose up -d
-sleep 10 # wait for database migrations
+docker-compose exec court_server /bin/sh -c "npx wait-on http://localhost:\$SERVER_METRICS_PORT"
 docker-compose exec -T court_server npm run test:server
 docker-compose exec -T court_server npm run test:services
 docker-compose down
