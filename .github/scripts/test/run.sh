83c6161ed9f941163f3958a95f2decd5d78bfd61
Martynas Prokopas <promaty@gmail.com>
2020-05-18 20:16:22 +0200

    minor test fixes

--- .github/scripts/test/run.sh
+++ .github/scripts/test/run.sh
@@ -5,7 +5,7 @@ cp .env.sample $DIR/.env
 cd $DIR
 export SERVER_IMAGE="$1"
 docker-compose up -d
-docker-compose exec court_server /bin/sh -c "npx wait-on http://localhost:\$SERVER_METRICS_PORT"
+docker-compose exec -T court_server /bin/sh -c "npx wait-on http://localhost:\$SERVER_METRICS_PORT --timeout 20000"
 docker-compose exec -T court_server npm run test:server
 docker-compose exec -T court_server npm run test:services
 docker-compose down
