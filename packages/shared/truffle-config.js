d560b5b87cb694949bb52387dffbffaef24a274e
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-06-03 14:16:13 -0300

    chore: drop usability instance

--- packages/shared/truffle-config.js
+++ packages/shared/truffle-config.js
@@ -2,15 +2,13 @@ const config = require('@aragon/truffle-config-v5/truffle-config')
 
 // copy rinkeby config for staging
 config.networks.staging = Object.assign({}, { ...config.networks.rinkeby })
-config.networks.usability = Object.assign({}, { ...config.networks.rinkeby })
 
-const { networks: { rpc, ropsten, rinkeby, usability, staging, mainnet } } = config
+const { networks: { rpc, ropsten, rinkeby, staging, mainnet } } = config
 
 rpc.court = undefined
 staging.court = '0x52180af656a1923024d1accf1d827ab85ce48878'
 ropsten.court = '0x3b26bc496aebaed5b3E0E81cDE6B582CDe71396e'
 rinkeby.court = '0xe9180dBE762Fe39520fC9883f7f7EFeBA6506534'
-usability.court = '0x44f788370206696b20B94BC77c4f73Ca264aa05E'
 mainnet.court = '0xee4650cBe7a2B23701D416f58b41D8B76b617797'
 
 module.exports = config
