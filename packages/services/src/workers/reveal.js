44036c20c7a7ec514d14a6eeafbce6666d35f0e2
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-09-03 09:19:54 -0300

    reveals: handle reveal period

--- packages/services/src/workers/reveal.js
+++ packages/services/src/workers/reveal.js
@@ -5,9 +5,13 @@ const REVEAL_TRIES = 3
 
 export default async function (ctx) {
   const { logger } = ctx
-  const reveals = await Reveal.query().where({ revealed: false }).where('failedAttempts', '<', REVEAL_TRIES).orderBy('createdAt', 'DESC')
-  logger.info(`${reveals.length} reveals pending`)
+  const reveals = await Reveal.query()
+    .where({ revealed: false })
+    .where({ expired: false })
+    .where('failedAttempts', '<', REVEAL_TRIES)
+    .orderBy('createdAt', 'DESC')
 
+  logger.info(`${reveals.length} reveals pending`)
   const court = await Network.getCourt()
   for (const instance of reveals) await reveal(logger, court, instance)
 }
@@ -16,6 +20,16 @@ async function reveal(logger, court, reveal) {
   const { voteId, juror, outcome, salt, failedAttempts } = reveal
   try {
     logger.info(`Revealing vote #${voteId} for juror ${juror}`)
+    const { disputeId, roundNumber } = reveal
+    const { canReveal, expired } = await court.getRevealStatus(disputeId, roundNumber)
+
+    if (expired) {
+      await reveal.$query().update({ expired: true })
+      return logger.error(`Reveal vote #${voteId} for juror ${juror} is expired!`)
+    } else if (!canReveal) {
+      return logger.warn(`Cannot reveal round #${roundNumber} for dispute #${disputeId} yet`)
+    }
+
     await court.revealFor(voteId, juror, outcome, salt)
     const actualOutcome = await court.getOutcome(voteId, juror)
     if (actualOutcome.toString() !== outcome.toString()) throw Error(`Expected outcome ${outcome}, got ${actualOutcome.toString()}`)
