ce3cadb6d77283a11d5f9d2f83a3232bd9e0c370
Martynas Prokopas <promaty@gmail.com>
2020-09-08 21:09:33 +0200

    chore: sending manual email for opened appeals

---
+++ emails/templates/appeals-confirm.js
@@ -0,0 +1,48 @@
+const {
+  addressBadge,
+  action,
+  base,
+  infobox,
+  link,
+  textFooter
+} = require('../template-utils')
+const { accountData } = require('../helpers')
+
+module.exports = function() {
+  return {
+    subject: '{{subject}}',
+    template: base(
+      {
+        title: 'Notifications',
+        subtitle: `Your account ${addressBadge()} received a notification on {{date}}`,
+      },
+      `
+        ${infobox({
+          mode: 'negative',
+          primary: `Aragon Court ${link('Dispute #20','https://court.aragon.org/#/disputes/20')} and ${link('Dispute #21','https://court.aragon.org/#/disputes/21')} have been APPEALED.`,
+          secondary: `Anyone can now CONFIRM the appeal to challenge the outcome proposed by the appealing party. If you disagree with the rulings proposed by the appealing party, visit the Aragon Court Dashboard to confirm these appeals: ${link(
+            'https://court.aragon.org/#/disputes',
+            'https://court.aragon.org/#/disputes'
+          )}`,
+        })}
+        ${action('Learn more about appeals', 'https://help.aragon.org/article/43-dispute-lifecycle#appeal', { padding: '16px 0 0' })}
+      `
+    ),
+    templateText: `
+      Aragon Court Notifications
+
+      Your account {{account}} received a notification on {{date}}:
+
+      {{subject}}
+      Anyone can now CONFIRM the appeal to challenge the outcome proposed by the appealing party. If you disagree with the rulings proposed by the appealing party, visit the Aragon Court Dashboard to confirm these appeals: https://court.aragon.org/#/disputes
+
+      Learn more about appeals: https://help.aragon.org/article/43-dispute-lifecycle#appeal
+      ${textFooter()}
+    `,
+    mockData: {
+      subject: 'Aragon Court Dispute #20 and Dispute #21 have been APPEALED.',
+      ...accountData('0xef0f7ecef8385483ac8a2e92d761f571c4b782bd'),
+      date: 'Thursday, 17 Dec. 2019',
+    },
+  }
+}
---
+++ packages/server/scripts/notify-appeals-confirm.js
@@ -0,0 +1,47 @@
+/**
+ * Usage: npx babel-node scripts/notify-appeals-confirm
+ * requires .env file / env vars with
+ * DB_NAME=
+ * DB_USER=
+ * DB_PASS=
+ * DB_PORT=
+ * DB_HOST=
+ * POSTMARK_SERVER_API_TOKEN=
+ */
+import { User } from '../src/models/objection'
+import emailClient from '@aragonone/court-backend-shared/helpers/email-client'
+import dotenv from 'dotenv'
+import { accountData } from '../../../emails/helpers'
+dotenv.config()
+
+async function main() {
+  const users = await User.findWithoutDisabledNotifications()
+  const emails = new Set()
+  for (const user of users) {
+    const { email: { email }, address } = user
+    if (emails.has(email.toLowerCase())) continue
+    emails.add(email.toLowerCase())
+    try {
+      await emailClient.sendEmailWithTemplate({
+        To: email,
+        From: 'notifications@court.aragon.org',
+        TemplateAlias: 'appeals-confirm',
+        TemplateModel: {
+          subject: 'Aragon Court Dispute #20 and Dispute #21 have been APPEALED.',
+          ...accountData(address),
+          date: 'Tuesday, 8 Sep. 2020'
+        },
+      })
+    } catch (err) {
+      console.log(err.message)
+    }
+    console.log(`sent email to ${email} for address ${address}`)
+  }
+  console.log(`Total emails: ${emails.size}`)
+  process.exit(0)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
