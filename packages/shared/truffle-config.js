170f3b9d687e4ec2218f16f562194720aebc6733
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2020-09-22 09:54:16 -0300

    CLI: Update rinkeby instance (#219)

--- packages/shared/truffle-config.js
+++ packages/shared/truffle-config.js
@@ -8,7 +8,7 @@ const { networks: { rpc, ropsten, rinkeby, staging, mainnet } } = config
 rpc.court = undefined
 staging.court = '0x52180af656a1923024d1accf1d827ab85ce48878'
 ropsten.court = '0x3b26bc496aebaed5b3E0E81cDE6B582CDe71396e'
-rinkeby.court = '0xe9180dBE762Fe39520fC9883f7f7EFeBA6506534'
+rinkeby.court = '0xf7970965489f4d4eafcf5fad55ca5ea57fb4bfcb'
 mainnet.court = '0xee4650cBe7a2B23701D416f58b41D8B76b617797'
 
 module.exports = config
