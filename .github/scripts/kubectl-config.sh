b67232bdd61771f72bb10342fa6f0baa365432ef
Cristopher <ortega.cpp@gmail.com>
2020-11-26 13:32:54 -0400

    configuration: update cluster info

--- .github/scripts/kubectl-config.sh
+++ .github/scripts/kubectl-config.sh
@@ -4,7 +4,7 @@ KUBE_SERVER="$2"
 KUBE_TOKEN="$3"
 # config cluster access
 echo $KUBE_CA | base64 -d > ca.crt
-kubectl config set-cluster aragon --server=$KUBE_SERVER --certificate-authority=ca.crt
-kubectl config set-credentials aragon --token=$(echo $KUBE_TOKEN | base64 -d)
-kubectl config set-context aragon --cluster=aragon --user=aragon
-kubectl config use-context aragon
+kubectl config set-cluster celeste --server=$KUBE_SERVER --certificate-authority=ca.crt
+kubectl config set-credentials celeste --token=$(echo $KUBE_TOKEN | base64 -d)
+kubectl config set-context celeste --cluster=celeste --user=celeste
+kubectl config use-context celeste
