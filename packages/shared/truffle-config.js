ce930b035ed00e9e3bfdf5b4667dfc4eec845771
Cristopher Ortega <ortega.cpp@gmail.com>
2021-02-24 10:18:40 -0400

    Update arbitrator address

--- packages/shared/truffle-config.js
+++ packages/shared/truffle-config.js
@@ -8,7 +8,7 @@ const { networks: { rpc, ropsten, rinkeby, staging, mainnet } } = config
 rpc.court = undefined
 staging.court = '0x52180af656a1923024d1accf1d827ab85ce48878'
 ropsten.court = '0x3b26bc496aebaed5b3E0E81cDE6B582CDe71396e'
-rinkeby.court = '0xdA4d0E51dD16D0b2A0baA81aE48D990926958B28'
+rinkeby.court = '0x7Ecb121a56BF92442289Dddb89b28A58640e76F5'
 mainnet.court = '0xee4650cBe7a2B23701D416f58b41D8B76b617797'
 
 module.exports = config
