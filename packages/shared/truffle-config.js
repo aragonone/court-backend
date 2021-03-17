5095519c6d8889aff23c510f072a18aa30ddd348
Cristopher Ortega <ortega.cpp@gmail.com>
2021-03-17 13:45:08 -0400

    Update rinkeby deployment addresses

--- packages/shared/truffle-config.js
+++ packages/shared/truffle-config.js
@@ -8,7 +8,7 @@ const { networks: { rpc, ropsten, rinkeby, staging, mainnet } } = config
 rpc.court = undefined
 staging.court = '0x52180af656a1923024d1accf1d827ab85ce48878'
 ropsten.court = '0x3b26bc496aebaed5b3E0E81cDE6B582CDe71396e'
-rinkeby.court = '0x7Ecb121a56BF92442289Dddb89b28A58640e76F5'
+rinkeby.court = '0x35e7433141D5f7f2EB7081186f5284dCDD2ccacE'
 mainnet.court = '0xee4650cBe7a2B23701D416f58b41D8B76b617797'
 
 module.exports = config
